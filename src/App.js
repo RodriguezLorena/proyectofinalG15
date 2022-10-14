b093ff6b512da1a322c444310ea4d42df2505841
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-10-14 10:51:39 -0300

    probando rutas delete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
-import Home from "./components/Home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+     
     </div>
   );
 }
