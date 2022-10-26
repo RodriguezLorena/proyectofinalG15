0f41a28dce9ad43b7818176e3b3b2938b4b0d9c9
liamgabriel2021 <arbey221@gmail.com>
2022-10-26 10:59:36 -0500

    hhh

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -88,7 +88,7 @@ export default function reducer(state = initialState, { payload, type }) {
     case "FILTER_SIZE":
       let filterSize = state.filterPrice;
       let filtrado =
-        payload == "all"
+        payload === "all"
           ? filterSize
           : filterSize.filter((e) => e.sizes.includes(payload));
 
@@ -113,19 +113,19 @@ export default function reducer(state = initialState, { payload, type }) {
       };
 
     case "FILTER_TYPE":
-
       let filterType = state.filterPrice;
-      let filtradoo = payload === "all"
+      let filtradoo =
+        payload === "all"
           ? filterType
-          : filterType.filter((e) => e.type === payload);   
-       console.log("ESTOY EN EL REDUCER ", filtradoo);
+          : filterType.filter((e) => e.type === payload);
+      console.log("ESTOY EN EL REDUCER ", filtradoo);
       let filterType2 = state.filterProducts;
       console.log("ESTOY EN EL REDUCER 2 ", filterType2);
       let filtradoo2 =
         payload === "all"
           ? filterType2
           : filterType2.filter((e) => e.type === payload);
-          console.log("ACA ESTA TYPE ",filtradoo2)
+      console.log("ACA ESTA TYPE ", filtradoo2);
       return {
         ...state,
         productsAll: filtradoo.length ? filtradoo : filtradoo2,
